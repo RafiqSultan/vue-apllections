d89fa99ab91ecce09578d7f815759977e3c5f509
RafiqSultan <Rafiqaasultan@gmail.com>
2022-09-04 06:28:58 -0700

    CREATE provide and inject

--- ComponentCommunication/src/main.js
+++ ComponentCommunication/src/main.js
@@ -4,12 +4,20 @@ import router from './router'
 import FriendContact from './components/FriendContact.vue';
 import NewFriend from './components/NewFriend.vue';
 import './assets/main.css'
+import ActiveElement from './components/ActiveElement.vue';
+import KnowledgeBase from './components/KnowledgeBase.vue';
+import KnowledgeElement from './components/KnowledgeElement.vue';
+import KnowledgeGrid from './components/KnowledgeGrid.vue';
 
 const app = createApp(App)
 
 app.use(router)
 app.component('friend-contact', FriendContact);
 app.component('new-friend', NewFriend);
+app.component('active-element', ActiveElement);
+app.component('knowledge-base', KnowledgeBase);
+app.component('knowledge-element', KnowledgeElement);
+app.component('knowledge-grid', KnowledgeGrid);
 
 
 
