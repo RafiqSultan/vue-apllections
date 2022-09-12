6de2699c8b49c71c10ce6eda951692629c918471
RafiqSultan <Rafiqaasultan@gmail.com>
2022-09-12 09:11:32 -0700

    CREATE resource-app project

--- vue-forms/src/components/TheForm.vue
+++ vue-forms/src/components/TheForm.vue
@@ -1,8 +1,18 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div
+      class="form-control"
+      :class="{ invalid: userNameValidity === 'invalid' }"
+    >
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="userName"
+        @blur="validateInput"
+      />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name!</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -108,6 +118,7 @@ export default {
       interest: [],
       how: null,
       confirm: false,
+      userNameValidity: "pending",
     };
   },
   methods: {
@@ -121,6 +132,13 @@ export default {
       console.log(this.interest);
       console.log(this.how);
       console.log(this.confirm);
+    },
+    validateInput() {
+      if (this.userName === "") {
+        this.userNameValidity = "invalid";
+      } else {
+        this.userNameValidity = "valid";
+      }
     },
   },
 };
@@ -137,6 +155,13 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  color: red;
 }
 
 label {
